b5fc34778621fee661cd93977492e27849969266
WoodenPig <50345914+WoodenPig@users.noreply.github.com>
2019-05-07 11:10:59 +0800

    Update 01-triangles.cpp

--- src/01-triangles/01-triangles.cpp
+++ src/01-triangles/01-triangles.cpp
@@ -28,7 +28,7 @@ static void checkGLError(const char* cmd){
 // init
 //
 void
-init( void )
+init( void )//顶点数据
 {
     glGenVertexArrays( NumVAOs, VAOs );
     glBindVertexArray( VAOs[Triangles] );
@@ -46,26 +46,67 @@ init( void )
           使用glBufferData()将顶点数据拷贝到缓存对象中。
     */
     glGenBuffers( NumBuffers, Buffers );
-    /*glGenBuffers()创建缓存对象并且返回缓存对象的标示符。它需要2个参数：
-        第一个为需要创建的缓存数量，
-        第二个为用于存储单一ID或多个ID的GLuint变量或数组的地址。*/
+    /*
+n
+    Specifies the number of buffer object names to be generated.
+buffers
+    Specifies an array in which the generated buffer object names are stored.
+Description
+
+        glGenBuffers returns n buffer object names in buffers. There is no guarantee that the names form a contiguous set 
+      of integers; however, it is guaranteed that none of the returned names was in use immediately before the call to 
+      glGenBuffers.
+
+        Buffer object names returned by a call to glGenBuffers are not returned by subsequent calls, unless they are first 
+      deleted with glDeleteBuffers.No buffer objects are associated with the returned buffer object names until they are 
+      first bound by calling glBindBuffer.
+   */
     glBindBuffer( GL_ARRAY_BUFFER, Buffers[ArrayBuffer] );
     /*
-        当缓存对象创建之后，在使用缓存对象之前，我们需要将缓存对象连接到相应的缓存上。glBindBuffer()有2个参数：target与buffer。
-        target告诉VBO该缓存对象将保存顶点数组数据还是索引数组数据：GL_ARRAY_BUFFER或GL_ELEMENT_ARRAY。任何顶点属性，如顶点坐标、
-        纹理坐标、法线与颜色分量数组都使用GL_ARRAY_BUFFER。用于glDraw[Range]Elements()的索引数据需要使用GL_ELEMENT_ARRAY绑定。
-        注意，target标志帮助VBO确定缓存对象最有效的位置，如有些系统将索引保存AGP或系统内存中，将顶点保存在显卡内存中。
-        当第一次调用glBindBuffer()，VBO用0大小的内存缓存初始化该缓存，并且设置VBO的初始状态，如用途与访问属性。
+        void glBindBuffer(GLenum target, GLuint buffer);
+
+target
+    Specifies the target buffer object. The symbolic constant must be GL_ARRAY_BUFFER, GL_ATOMIC_COUNTER_BUFFER, 
+    GL_COPY_READ_BUFFER, GL_COPY_WRITE_BUFFER, GL_DRAW_INDIRECT_BUFFER, GL_DISPATCH_INDIRECT_BUFFER, GL_ELEMENT_ARRAY_BUFFER, 
+    GL_PIXEL_PACK_BUFFER, GL_PIXEL_UNPACK_BUFFER, GL_QUERY_BUFFER, GL_SHADER_STORAGE_BUFFER, GL_TEXTURE_BUFFER, 
+    GL_TRANSFORM_FEEDBACK_BUFFER, or GL_UNIFORM_BUFFER.
+buffer
+    Specifies the name of a buffer object.
+Description
+glBindBuffer binds a buffer object to the specified buffer binding point. Calling glBindBuffer with target​ set to one of 
+the accepted symbolic constants and buffer set to the name of a buffer object binds that buffer object name to the target. 
+If no buffer object with name buffer exists, one is created with that name. When a buffer object is bound to a target, the 
+previous binding for that target is automatically broken.
     */
     //glBufferStorage is API of openGL4.5
     //glBufferStorage( GL_ARRAY_BUFFER, sizeof(vertices), vertices, 0);
     glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices,  GL_STATIC_DRAW);
     /*
-        当缓存初始化之后，你可以使用glBufferData()将数据拷贝到缓存对象。
-        第一个参数target可以为GL_ARRAY_BUFFER或GL_ELEMENT_ARRAY。size为待传递数据字节数量。第三个参数为源数据数组指针，如data为NULL，
-        则VBO仅仅预留给定数据大小的内存空间。最后一个参数usage标志位VBO的另一个性能提示，它提供缓存对象将如何使用：
-        static、dynamic或stream、与read、copy或draw。
+        void glBufferData(GLenum target​, GLsizeiptr size​, const GLvoid * data​, GLenum usage​);
+
+target
+    Specifies the target buffer object. The symbolic constant must be GL_ARRAY_BUFFER, GL_ATOMIC_COUNTER_BUFFER, 
+    GL_COPY_READ_BUFFER, GL_COPY_WRITE_BUFFER, GL_DRAW_INDIRECT_BUFFER, GL_DISPATCH_INDIRECT_BUFFER, GL_ELEMENT_ARRAY_BUFFER, 
+    GL_PIXEL_PACK_BUFFER, GL_PIXEL_UNPACK_BUFFER, GL_QUERY_BUFFER, GL_SHADER_STORAGE_BUFFER, GL_TEXTURE_BUFFER,
+    GL_TRANSFORM_FEEDBACK_BUFFER, or GL_UNIFORM_BUFFER.
+
+size
+    Specifies the size in bytes of the buffer object's new data store.
+data
+    Specifies a pointer to data that will be copied into the data store for initialization, or NULL if no data is to be copied.
+usage
+    Specifies the expected usage pattern of the data store. The symbolic constant must be GL_STREAM_DRAW, GL_STREAM_READ, 
+    GL_STREAM_COPY, GL_STATIC_DRAW, GL_STATIC_READ, GL_STATIC_COPY, GL_DYNAMIC_DRAW, GL_DYNAMIC_READ, or GL_DYNAMIC_COPY.
+
+Description
+
+glBufferData creates a new data store for the buffer object currently bound to target​. Any pre-existing data store is deleted. 
+The new data store is created with the specified size​ in bytes and usage​. If data​ is not NULL, the data store is initialized 
+with data from this pointer. In its initial state, the new data store is not mapped, it has a NULL mapped pointer, and its 
+mapped access is GL_READ_WRITE.
     */
+    
+    //*********************************************顶点着色器*****************************************************
     ShaderInfo  shaders[] =
     {
         { GL_VERTEX_SHADER, "media/shaders/triangles/triangles.vert" },
@@ -94,7 +135,32 @@ display( void )
     glClearBufferfv(GL_COLOR, 0, black);
 
     glBindVertexArray( VAOs[Triangles] );
+    /*
+        
+array
+    Specifies the name of the vertex array to bind.
+
+Description
+
+        glBindVertexArray binds the vertex array object with name array​. array​ is the name of a vertex array object 
+    previously returned from a call to glGenVertexArrays, or zero to break the existing vertex array object binding.
+    If no vertex array object with name array​ exists, one is created when array​ is first bound. If the bind is successful 
+    no change is made to the state of the vertex array object, and any previous vertex array object binding is broken.
+Errors
+    GL_INVALID_OPERATION is generated if array​ is not zero or the name of a vertex array object previously returned from 
+    a call to glGenVertexArrays.
+   */
     glDrawArrays( GL_TRIANGLES, 0, NumVertices );
+    /*      
+mode
+    Specifies what kind of primitives to render. Symbolic constants GL_POINTS, GL_LINE_STRIP, GL_LINE_LOOP, GL_LINES, 
+    GL_LINE_STRIP_ADJACENCY, GL_LINES_ADJACENCY, GL_TRIANGLE_STRIP, GL_TRIANGLE_FAN, GL_TRIANGLES, GL_TRIANGLE_STRIP_ADJACENCY,
+    GL_TRIANGLES_ADJACENCY and GL_PATCHES are accepted.
+first
+    Specifies the starting index in the enabled arrays.
+count
+    Specifies the number of indices to be rendered. 
+    */
 }
 
 //----------------------------------------------------------------------------
